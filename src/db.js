a6069877a6fd1d5263f86bceb1c0c00f6bc763f1
Pasha <pashket.joller@gmail.com>
2024-12-09 00:04:45 +0300

    db1

--- src/db.js
+++ src/db.js
@@ -11,4 +11,16 @@ const pool = new Pool({
   port: 5432,                 // порт PostgreSQL
 });
 
-module.exports = pool;
+async function testConnection() {
+    try {
+      // Пробуем выполнить простой запрос
+      const res = await pool.query('SELECT NOW()');
+      console.log('Подключение успешно:', res.rows[0]);
+    } catch (err) {
+      console.error('Ошибка подключения:', err);
+    } finally {
+      pool.end(); // Закрываем подключение
+    }
+  }
+  
+  testConnection();
